bbff9d3400fa4b0c356af3d8100a9c5a4db17eb4
likorsi <likorsi.sl@gmail.com>
2022-03-14 22:33:32 +0300

    lab3

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -13,6 +13,9 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 
 import static org.hamcrest.Matchers.allOf;
 
+import android.Manifest;
+import android.os.Environment;
+
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
@@ -20,37 +23,89 @@ import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AllureAndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/screenshots/" );
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        } };
 
     @Before
     public void registerIdlingResources() {
         IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
     }
 
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
 
     @Test
     public void positiveTest() {
+        Allure.feature("positiveTest");
         onView(withId(R.id.text_home)).check(matches(withText("This is home fragment")));
     }
 
     @Test
     public void negativeTest() {
+        Allure.feature("negativeTest");
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.close());
@@ -61,6 +116,7 @@ public class ExampleInstrumentedTest {
 
     @Test
     public void intentTest() {
+        Allure.feature("intentTest");
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         Intents.init();
         onView(withText(R.string.action_settings)).perform(click());
@@ -70,6 +126,7 @@ public class ExampleInstrumentedTest {
 
     @Test
     public void listTest() {
+        Allure.feature("listTest");
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.close());
@@ -79,10 +136,5 @@ public class ExampleInstrumentedTest {
         recyclerView.check(matches((CustomViewMatcher.recyclerViewSizeMatcher(10))));
 
         onView(allOf(withId(R.id.item_number), withText("1"))).check(matches(isDisplayed()));
-    }
-
-    @After
-    public void unregisterIdlingResources() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
     }
 }
